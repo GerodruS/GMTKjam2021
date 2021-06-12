af1a7993e43954c8eaeabee0b6a37f816fe3cc6a
gerodrus <gerodrus@gmail.com>
2021-06-12 15:01:47 +0300

    Drawing level layout

--- src/main.rs
+++ src/main.rs
@@ -15,15 +15,11 @@ async fn main() {
 
     let mut game_state = GameState::Start;
 
-    let mut camera = Camera2D {
-        target: Vec2::ZERO,
-        ..Default::default()
-    };
+    let mut camera = Default::default();
     set_camera(&camera);
 
     'game_loop: loop {
         clear_background(BLACK);
-        update_screen_size(&mut camera, &game_data);
 
         match game_state {
             GameState::Start => {
@@ -52,10 +48,52 @@ async fn main() {
             GameState::Level { level_index } => {
                 let level_data = &(game_data.levels[level_index]);
                 let layout = level_data.layouts[0].clone();
-                // let layout = layout.trim_matches(|c| c == '\n');
-                // let layout = layout.replace(" ", "");
-                // println!("s\n[{}]\n[{}]", level_data.layouts[0].as_str(), layout);
 
+                let mut width = 0;
+                let mut height = 0;
+                let mut start_position = -Vec2::ONE;
+                let mut finish_position = -Vec2::ONE;
+                for line in layout.split(|c| c == '\n' || c == ' ') {
+                    let line_width = line.len();
+                    if 0 < line_width {
+                        width = width.max(line_width);
+                        height += 1;
+                        match line.chars().position(|c| c == 's') {
+                            Some(i) => {
+                                if start_position == -Vec2::ONE {
+                                    start_position.x = i as f32;
+                                    start_position.y = (height - 1) as f32;
+                                } else {
+                                    // TODO: second start_position -- not good
+                                }
+                            }
+                            _ => {}
+                        }
+                        match line.chars().position(|c| c == 'f') {
+                            Some(i) => {
+                                if finish_position == -Vec2::ONE {
+                                    finish_position.x = i as f32;
+                                    finish_position.y = (height - 1) as f32;
+                                } else {
+                                    // TODO: second finish_position -- not good
+                                }
+                            }
+                            _ => {}
+                        }
+                    }
+                }
+                // println!("w={} h={} s={:} f={:}", width, height, start_position, finish_position);
+                update_screen_size(&mut camera, (width as f32, height as f32));
+
+                draw_rectangle_lines(-0.5, -0.5, width as f32, height as f32, 0.1, GRAY);
+                for y in 0..height {
+                    for x in 0..width {
+                        draw_circle(x as f32, y as f32, 0.1, GRAY);
+                    }
+                }
+
+                draw_circle(start_position.x, start_position.y, 0.5, YELLOW);
+                draw_circle(finish_position.x, finish_position.y, 0.5, GREEN);
 
                 egui_macroquad::ui(|egui_ctx| {
                     egui::Window::new("GMTK Game Jam 2021").show(egui_ctx, |ui| {
@@ -82,25 +120,26 @@ async fn main() {
     }
 }
 
-pub fn update_screen_size(camera: &mut Camera2D, game_data: &GameData) {
+pub fn update_screen_size(camera: &mut Camera2D, virtual_size: (f32, f32)) {
     // TODO: add delay and test for new screen size
 
     let real_screen_size = vec2(screen_width(), screen_height());
 
     let real_aspect_ratio = real_screen_size.x / real_screen_size.y;
-    let target_aspect_ratio = game_data.resolution.0 / game_data.resolution.1;
+    let target_aspect_ratio = virtual_size.0 / virtual_size.1;
     let virtual_screen_size = if target_aspect_ratio < real_aspect_ratio {
         vec2(
-            real_aspect_ratio * game_data.resolution.1,
-            game_data.resolution.1,
+            real_aspect_ratio * virtual_size.1,
+            virtual_size.1,
         )
     } else {
         vec2(
-            game_data.resolution.0,
-            game_data.resolution.0 / real_aspect_ratio,
+            virtual_size.0,
+            virtual_size.0 / real_aspect_ratio,
         )
     };
 
     camera.zoom = vec2(2.0 / virtual_screen_size.x, -2.0 / virtual_screen_size.y);
+    camera.target = (vec2(virtual_size.0, virtual_size.1) - Vec2::ONE) / 2.0;
     set_camera(camera);
 }
