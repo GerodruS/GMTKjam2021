5ae7555237e692e3f342672c104b1201e559e633
gerodrus <gerodrus@gmail.com>
2021-06-12 12:00:21 +0300

    game_data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+// use ron::ser::{to_string_pretty, PrettyConfig};
+// use serde::{Serialize, Deserialize};
+// use std::{collections::HashMap, iter::FromIterator};
+// use std::fs::File;
+// use std::io::prelude::*;
+
+mod game_data;
+use game_data::GameData;
+// use std::io::Error;
+
 fn main() {
-    println!("Hello, world!");
+    let file_name = "assets/game.data";
+    let game_data = GameData::load_from_file(file_name).expect("Game data not read");
+    println!("[{}] [{}] [{}]", game_data.levels[0], game_data.levels[1], game_data.levels[2]);
 }
